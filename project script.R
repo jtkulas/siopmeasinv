47f5db28d5bfb4d5121d74c9bbd18f7969a6869c
jtkulas <kulasj@montclair.edu>
2020-09-04 09:13:47 -0400

    careless response indicated

--- project script.R
+++ project script.R
@@ -1,5 +1,9 @@
 library(Hmisc)
+library(careless)
 
 data <- spss.get("TD-12 Research 2020 ADDITIONAL.sav")
 
 write.csv(data, "SIOP.csv")
+
+table <- longstring(data[2:13], avg = FALSE) ## then sort and screen
+
