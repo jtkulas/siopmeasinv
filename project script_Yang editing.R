4e8c891b79260f1017249165e9a873d7216bf263
yangyangsh <yangyangsh@outlook.com>
2021-07-15 22:09:46 +0800

    ## revise

--- project script_Yang editing.R
+++ project script_Yang editing.R
@@ -1,15 +1,16 @@
 library(foreign)
 library(careless)
 
-getwd()
 
 masterdata <- read.spss("C://Users//yangy//Documents//Git//siopmeasinv//Dirty Dozon data for SIOP.sav")
-DDdata <- data.frame(masterdata[c(1:3,34:46)])
+DDdata <- data.frame(masterdata[c(1:3,34:45)])
 
-careless_long <- longstring(DDdata[4:16])
-careless_irv <- irv(DDdata[4:16])
+careless_long <- longstring(DDdata[4:15])
+careless_irv <- irv(DDdata[4:15])
 
 careless_long <- data.frame(careless_long)
 careless_irv  <- data.frame(careless_irv)
 
 DDdata <- cbind(DDdata, careless_long, careless_irv)
+
+##test
