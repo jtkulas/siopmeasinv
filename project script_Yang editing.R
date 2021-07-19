afe8c0ee1c94b8e6387f2c213e9798dd9d21cc7f
jtkulas <kulasj@montclair.edu>
2021-07-19 10:46:45 -0500

    paper template and initial data screening

--- project script_Yang editing.R
+++ project script_Yang editing.R
@@ -2,8 +2,12 @@ library(foreign)
 library(careless)
 
 
-masterdata <- read.spss("C://Users//yangy//Documents//Git//siopmeasinv//Dirty Dozon data for SIOP.sav")
+masterdata <- read.spss("Dirty Dozon data for SIOP.sav")
 DDdata <- data.frame(masterdata[c(1:3,34:45)])
+
+psych::describe(DDdata)
+table <- descr::freq(DDdata$selection)
+dtable <- cbind(sample = rownames(table), table)
 
 careless_long <- longstring(DDdata[4:15])
 careless_irv <- irv(DDdata[4:15])
@@ -13,4 +17,8 @@ careless_irv  <- data.frame(careless_irv)
 
 DDdata <- cbind(DDdata, careless_long, careless_irv)
 
-##test
+descr::freq(DDdata$careless_long)
+descr::freq(DDdata$careless_irv)
+
+use <- DDdata[ which(DDdata$careless_long<7), ]
+
